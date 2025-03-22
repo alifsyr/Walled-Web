417e35215092e9c63234874d24c5e3c087ea8beb
alifsyr <alifsyr@gmail.com>
2025-03-22 15:51:17 +0700

    Feat: Update routing in App component to include SignUp and TopUp pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import './App.css'
-import LoginPage from './pages/Login'
 import { Routes, Route } from 'react-router-dom'
+
+import './App.css'
+
+import LoginPage from './pages/Login'
+import SignUpPage from './pages/SignUp'
+import TopUp from './pages/TopUp'
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />\
+        <Route path="/top-up" element={<TopUp />} />
       </Routes>
     </div>
   )
