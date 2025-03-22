09809ab044f0473b1cea321e95f5e63992be2cf2
alifsyr <alifsyr@gmail.com>
2025-03-22 19:36:41 +0700

    Feat: Implement public route protection for login, signup, and dashboard pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 import './App.css'
 
@@ -9,19 +9,52 @@ import Transfer from './pages/Transfer'
 import Dashboard from './pages/Dashboard'
 import NotFound from './pages/NotFound'
 
+const ProtectedRoute = ({ children }) => {
+    const isAuthenticated = localStorage.getItem('authToken')
+    return isAuthenticated ? children : <Navigate to="/login" />
+}
+
+const PublicRoute = ({ children }) => {
+    const isAuthenticated = localStorage.getItem('authToken')
+    return !isAuthenticated ? children : <Navigate to="/" />
+}
+
 function App() {
-  return (
-    <div>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/top-up" element={<TopUp />} />
-        <Route path="/transfer" element={<Transfer />} />
-        <Route path="/" element={<Dashboard />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
-  )
+    return (
+        <div>
+            <Routes>
+                <Route path="/login" element={
+                    <PublicRoute>
+                        <LoginPage />
+                    </PublicRoute>
+                } />
+                <Route path="/signup" element={
+                    <PublicRoute>
+                        <SignUpPage />
+                    </PublicRoute>
+                } />
+
+                <Route path="/" element={
+                    <PublicRoute>
+                        <Dashboard />
+                    </PublicRoute>
+                } />
+                <Route path="/top-up" element={
+                    <PublicRoute>
+                        <TopUp />
+                    </PublicRoute>
+                } />
+                <Route path="/transfer" element={
+                    <PublicRoute>
+                        <Transfer />
+                    </PublicRoute>
+                } />
+
+                {/* Not Found Route */}
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </div>
+    )
 }
 
 export default App
